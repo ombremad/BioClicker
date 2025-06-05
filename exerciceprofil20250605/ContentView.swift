78888a47c178026124cbb4de8a7acc06d4d12d08
Anne Ferret <anne@anneferret.eu>
2025-06-05 11:45:39 +0200

    Initial Commit

---
+++ exerciceprofil20250605/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  exerciceprofil20250605
+//
+//  Created by Anne Ferret on 05/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ exerciceprofil20250605/exerciceprofil20250605App.swift
@@ -0,0 +1,17 @@
+//
+//  exerciceprofil20250605App.swift
+//  exerciceprofil20250605
+//
+//  Created by Anne Ferret on 05/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct exerciceprofil20250605App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
